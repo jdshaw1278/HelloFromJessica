1f42cd7bb6ce07d58c96716d3ea458edf30f3227
Elijah_Bjork <bjo20003@byui.edu>
2021-07-16 16:49:11 -0600

    Commit from Elijah

--- src/shaw/Main.java
+++ src/shaw/Main.java
@@ -3,6 +3,9 @@ package shaw;
 public class Main {
 
     public static void main(String[] args) {
-	    System.out.println("Hello, my name is Jessica");
+
+        System.out.println("Hello, my name is Jessica");
+
+        System.out.println("Hello Jessica, my name is Elijah.");
     }
 }
